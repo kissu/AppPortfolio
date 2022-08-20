8dd3711a9bc4d24d7b5eb84d4cc34054ef0d1323
Paul Bouigue <pbouigue@users.noreply.github.com>
2022-08-20 21:46:34 +0200

    news!

---
+++ components/List-item.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <div
+      :class="{ active: isActive }"
+      @click="toggle"
+    >
+      <h2 class="title">
+        <slot name="title" />
+      </h2>
+
+      <div v-show="show" :class="{ active: isActive }" class="content">
+        <slot name="content" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      isActive: false,
+      show: true
+    }
+  },
+  methods: {
+    myFiler () {
+
+    },
+    toggle () {
+      // this.show = !this.show
+      this.isActive = !this.isActive
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,74 @@
 <template>
-  <Tutorial />
-</template>
+  <div>
+    <div v-for="accordion in accordions" :key="accordion.title">
+      <Item>
+        <template #title>
+          {{ accordion.title }}
+        </template>
 
+        <template #content>
+          <div>{{ accordion.text }}</div>
+        </template>
+      </Item>
+    </div>
+  </div>
+</template>
 <script>
+import Item from '../components/List-item.vue'
 export default {
-  name: 'IndexPage'
+  components: { Item },
+  data () {
+    return {
+      accordions: [
+        {
+          title: 'Il écrit',
+          text: 'Projects from il écrit'
+        },
+        {
+          title: 'Il chante',
+          text: 'Projects from il chante'
+        },
+        {
+          title: 'Il danse',
+          text: 'Projects from il danse !'
+        }
+      ]
+    }
+  }
+
 }
 </script>
+
+<style>
+body{
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: normal;
+
+}
+
+h2{
+  font-weight: normal;
+  font-size: 1em;
+  margin: 0;
+}
+
+.wrapper{
+    overflow: hidden;
+    max-height: 500px;
+    transition: 0.3s;
+        transition: all 1s linear;
+
+  }
+.content.active{
+  background: red;
+ max-height: 500px;
+    transition: max-height 1s ease-in;
+    }
+
+.content{
+  max-height: 0;
+    transition: max-height 1s ease-out;
+
+}
+
+</style>
